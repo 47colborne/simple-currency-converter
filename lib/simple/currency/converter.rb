e76e953f798cdb5d068f1573d7028b553bf1617e
jeffli <jeff.li@retailcommon.com>
2015-09-09 10:05:28 -0400

    created empty gem files

---
+++ lib/simple/currency/converter.rb
@@ -0,0 +1,9 @@
+require "simple/currency/converter/version"
+
+module Simple
+  module Currency
+    module Converter
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/simple/currency/converter/version.rb
@@ -0,0 +1,7 @@
+module Simple
+  module Currency
+    module Converter
+      VERSION = "0.0.1"
+    end
+  end
+end
